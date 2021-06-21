d5e885fcd81b9faeba1e7a2e769c3330ea93db23
James Meegan <jmeegan2@owlmail.harford.edu>
2021-06-21 03:31:53 -0400

    done for today

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 import TodoList from './TodoList'
 
 function App() {
+  const [todos, setTodos] = useState([])
   return (
+      <>
       <TodoList />
+      <input type="text" /> 
+      <button>Add ToDo</button>
+      <button>Clear Completed ToDos</button>
+      <div>0 Left to do</div>
+      </>
   )
 }
  
